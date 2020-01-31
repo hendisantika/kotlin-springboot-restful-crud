94dd4b95a299205729accb2f979f4b3697304919
Hendi Santika <hendisantika@gmail.com>
2020-02-01 05:13:08 +0700

    #14 - Add updateUserById in UserServiceImpl

--- src/main/kotlin/com/hendisantika/kotlinspringbootrestfulcrud/service/UserServiceImpl.kt
+++ src/main/kotlin/com/hendisantika/kotlinspringbootrestfulcrud/service/UserServiceImpl.kt
@@ -3,7 +3,9 @@ package com.hendisantika.kotlinspringbootrestfulcrud.service
 import com.hendisantika.kotlinspringbootrestfulcrud.model.User
 import com.hendisantika.kotlinspringbootrestfulcrud.repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.sql.Timestamp
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,6 +33,18 @@ class UserServiceImpl : UserService {
     override fun getUserById(userId: Long): ResponseEntity<User> {
         return userRepository.findById(userId).map { article ->
             ResponseEntity.ok(article)
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    override fun updateUserById(userId: Long, newUser: User): ResponseEntity<User> {
+        return userRepository.findById(userId).map { existingUser ->
+            val updatedUser: User = existingUser
+                    .copy(
+                            created = Timestamp(System.currentTimeMillis()),
+                            username = newUser.username,
+                            password = newUser.password
+                    )
+            ResponseEntity.ok().body(userRepository.save(updatedUser))
         }.orElse(ResponseEntity.notFound().build())
     }
 }
