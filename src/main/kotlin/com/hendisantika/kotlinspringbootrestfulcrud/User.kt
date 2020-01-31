38408b058453e4b6ed938c1376194a4523e2c266
Hendi Santika <hendisantika@gmail.com>
2020-02-01 05:07:26 +0700

    #6 - Add User Model

---
+++ src/main/kotlin/com/hendisantika/kotlinspringbootrestfulcrud/User.kt
@@ -0,0 +1,29 @@
+package com.hendisantika.kotlinspringbootrestfulcrud
+
+import java.sql.Timestamp
+import javax.persistence.*
+import javax.validation.constraints.NotBlank
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-springboot-restful-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/02/20
+ * Time: 05.05
+ */
+@Entity
+@Table(name = "users")
+data class User(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long = 0,
+
+        val created: Timestamp = Timestamp(System.currentTimeMillis()),
+
+        @get: NotBlank
+        val password: String = "",
+
+        @get: NotBlank
+        val username: String = ""
+)
