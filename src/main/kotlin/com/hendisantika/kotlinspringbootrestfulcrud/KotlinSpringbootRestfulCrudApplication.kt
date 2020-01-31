dd50859936cbc200d71dc20252c7c5c66edd1b5c
Hendi Santika <hendisantika@gmail.com>
2020-02-01 05:05:19 +0700

    #5 - Add SpringBootServletInitializer in Main App

--- src/main/kotlin/com/hendisantika/kotlinspringbootrestfulcrud/KotlinSpringbootRestfulCrudApplication.kt
+++ src/main/kotlin/com/hendisantika/kotlinspringbootrestfulcrud/KotlinSpringbootRestfulCrudApplication.kt
@@ -1,10 +1,16 @@
 package com.hendisantika.kotlinspringbootrestfulcrud
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class KotlinSpringbootRestfulCrudApplication
+class KotlinSpringbootRestfulCrudApplication : SpringBootServletInitializer() {
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(KotlinSpringbootRestfulCrudApplication::class.java)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<KotlinSpringbootRestfulCrudApplication>(*args)
